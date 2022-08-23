9ec1d32542accd5f402d53e081bc4ca8a4e7bf3c
Amirul Mu'minin <muminin71@gmail.com>
2022-08-23 16:02:30 +0700

    8. Memahami Blade Komponen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,19 @@
 @extends('layouts.app', ['title' => 'Homepage'])
 
 @section('content')
-<h1>Home</h1>
-<p>
-  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor provident, numquam totam inventore impedit rem
-  quisquam voluptatibus consequatur. Voluptas doloremque nulla magni perspiciatis. Obcaecati ratione id odio aliquid
-  et illum!
-</p>
+  <x-alert>
+    @slot('title')
+      Header component with <strong>strong</strong> tag
+    @endslot
+
+    Ini adalah children
+
+    @slot('footer')
+      Footer
+    @endslot
+  </x-alert>
+
+  <p>
+    This is homepage
+  </p>
 @endsection
