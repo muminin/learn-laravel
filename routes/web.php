9885c7e226ffcb7cb1a8e93fa72fd9c75bf469d7
Amirul Mu'minin <muminin71@gmail.com>
2022-08-24 11:30:54 +0700

    14. Passing request dari Controller

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileInformationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class);
 
 Route::get('contact', [ContactController::class, 'create']);
 Route::post('contact', [ContactController::class, 'store']);
+
+Route::get('profile/{name}', ProfileInformationController::class);
